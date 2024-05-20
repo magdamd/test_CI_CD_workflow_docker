80602b80a081a1959a3e477c6e3533cfff1bc2a4
magdamd <49106367+magdamd@users.noreply.github.com>
2024-05-20 15:02:08 +0200

    Add files via upload

--- PlaygroundMR0/Comparison_results_workflow.py
+++ PlaygroundMR0/Comparison_results_workflow.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-vectorGroundTruth = np.loadtxt("./PlaygroundMR0/results_GroundTruth/arrayX.txt")
-vectorLastPush = np.loadtxt("./PlaygroundMR0/results/arrayX.txt")
+vectorGroundTruth = np.loadtxt("./PlaygroundMR0/results_GroundTruth/FID_signal.txt")
+vectorLastPush = np.loadtxt("./PlaygroundMR0/results/FID_signal.txt")
 
 class TestPythonScript:
 
